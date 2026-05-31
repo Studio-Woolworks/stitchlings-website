8616b94df815a22c75b3cbf34403107cbfcd9dbc
maozblan <lyssa.li137@gmail.com>
2026-05-30 17:37:24 -0700

    header, footer, parallax setup

---
+++ js/components.js
@@ -0,0 +1,59 @@
+/// navigation bar
+class NavBar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+			<nav class="row">
+				<div>
+					<a href="/">Home</a>
+					<a href="/studio-woolworks">Studio Woolworks</a>
+				</div>
+
+				<div>
+					<a href="/faq">FAQ</a>
+					<a href="/press-kit">Press Kit</a>
+				</div>
+			</nav>
+    `;
+  }
+}
+customElements.define('nav-bar', NavBar);
+
+/// footer
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+			<footer class="col">
+				<div class="row">
+					<img src="/assets/imgs/footer/logo.png" alt="Stitchlings Logo" />
+
+					<div class="col">
+					 	<h3>Stay up to date!</h3>
+						<div class="row icons">
+							<a href="https://www.linkedin.com/company/stitchlings" target="_blank"><img src="/assets/imgs/footer/linkedin.png" /></a>
+							<a href="https://www.instagram.com/play.stitchlings" target="_blank"><img src="/assets/imgs/footer/instagram.png" /></a>
+							<a href="https://www.tiktok.com/@play.stitchlings" target="_blank"><img src="/assets/imgs/footer/tiktok.png" /></a>
+						</div>
+					</div>
+
+					<div class="col">
+						<h3>Join our community!</h3>
+						<div class="row icons">
+							<a href="https://discord.gg/GjAj5Kagcu" target="_blank"><img src="/assets/imgs/footer/discord.png" /></a>
+						</div>
+					</div>
+				</div>
+
+				<p>&copy; Copyright 2026 USC Games & Studio Woolworks</p>
+			</footer>
+    `;
+  }
+}
+customElements.define('stitch-footer', Footer);
+
+/// parallax backgrounds 
+window.addEventListener('scroll', () => {
+  const y = window.scrollY;
+
+  document.querySelector('#parallax')
+    .style.transform = `translateY(${y * 0.21}px)`;
+});
