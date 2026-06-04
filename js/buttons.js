29022c23ea2ae40af99a073f44b66357b5bd55aa
maozblan <lyssa.li137@gmail.com>
2026-06-04 07:35:24 -0700

    footer updates (back to top, border)

---
+++ js/buttons.js
@@ -0,0 +1,6 @@
+document.getElementById("back-to-top-btn").addEventListener("btn-click", () => {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
+});
--- js/components.js
+++ js/components.js
@@ -1,63 +1,96 @@
+/// sprite swap button
+class SpriteButton extends HTMLElement {
+	connectedCallback() {
+		const base = this.hasAttribute("base");
+		const path = base ? "." : "..";
+
+		const id = this.getAttribute("id");
+		const imgId = this.getAttribute("imgId");
+
+		this.innerHTML = `
+			<button class="sprite-swap-btn" id="${id}">
+				<img src="${path}/assets/imgs/buttons/${imgId}_norm.png" />
+				<img src="${path}/assets/imgs/buttons/${imgId}_hover.png" />
+			</button>
+		`;
+
+		this.addEventListener("click", function () {
+			this.dispatchEvent(new CustomEvent(`btn-click`));
+		});
+	}
+}
+customElements.define("sprite-btn", SpriteButton);
+
 /// navigation bar
 class NavBar extends HTMLElement {
-  connectedCallback() {
-		const base = this.hasAttribute('base');
+	connectedCallback() {
+		const base = this.hasAttribute("base");
+		const path = base ? "." : "..";
 
-    this.innerHTML = `
+		this.innerHTML = `
 			<nav class="row">
 				<div>
-					<a href="${base ? '.' : '..'}/">Home</a>
-					<a href="${base ? '.' : '..'}/studio-woolworks">Studio Woolworks</a>
+					<a href="${path}/">Home</a>
+					<a href="${path}/studio-woolworks">Studio Woolworks</a>
 				</div>
 
 				<div>
-					<a href="${base ? '.' : '..'}/faq">FAQ</a>
-					<a href="${base ? '.' : '..'}/press-kit">Press Kit</a>
+					<a href="${path}/faq">FAQ</a>
+					<a href="${path}/press-kit">Press Kit</a>
 				</div>
 			</nav>
     `;
-  }
+	}
 }
-customElements.define('nav-bar', NavBar);
+customElements.define("nav-bar", NavBar);
 
 /// footer
 class Footer extends HTMLElement {
-  connectedCallback() {
-		const base = this.hasAttribute('base');
+	connectedCallback() {
+		const base = this.hasAttribute("base");
+		const path = base ? "." : "..";
 
-    this.innerHTML = `
+		this.innerHTML = `
 			<footer class="col">
+				<img id="footer-top" src="${path}/assets/imgs/footer/footerTop.png" />
 				<div class="row">
-					<img src="${base ? '.' : '..'}/assets/imgs/footer/logo.png" alt="Stitchlings Logo" />
+					<img src="${path}/assets/imgs/footer/logo.png" alt="Stitchlings Logo" />
 
 					<div class="col">
 					 	<h3>Stay up to date!</h3>
 						<div class="row icons">
-							<a href="https://www.linkedin.com/company/stitchlings" target="_blank"><img src="${base ? '.' : '..'}/assets/imgs/footer/linkedin.png" /></a>
-							<a href="https://www.instagram.com/play.stitchlings" target="_blank"><img src="${base ? '.' : '..'}/assets/imgs/footer/instagram.png" /></a>
-							<a href="https://www.tiktok.com/@play.stitchlings" target="_blank"><img src="${base ? '.' : '..'}/assets/imgs/footer/tiktok.png" /></a>
+							<a href="https://www.linkedin.com/company/stitchlings" target="_blank">
+								<img src="${path}/assets/imgs/footer/linkedin.png" />
+							</a>
+							<a href="https://www.instagram.com/play.stitchlings" target="_blank">
+								<img src="${path}/assets/imgs/footer/instagram.png" />
+							</a>
+							<a href="https://www.tiktok.com/@play.stitchlings" target="_blank">
+								<img src="${path}/assets/imgs/footer/tiktok.png" />
+							</a>
 						</div>
 					</div>
 
 					<div class="col">
 						<h3>Join our community!</h3>
 						<div class="row icons">
-							<a href="https://discord.gg/GjAj5Kagcu" target="_blank"><img src="${base ? '.' : '..'}/assets/imgs/footer/discord.png" /></a>
+							<a href="https://discord.gg/GjAj5Kagcu" target="_blank"><img src="${path}/assets/imgs/footer/discord.png" /></a>
 						</div>
 					</div>
 				</div>
 
-				<p>&copy; Copyright 2026 USC Games & Studio Woolworks</p>
+				<p id="copyright">&copy; Copyright 2026 USC Games & Studio Woolworks</p>
+				<sprite-btn id="back-to-top-btn" imgId="backToTop" ${base ? "base" : ""}></sprite-btn>
 			</footer>
     `;
-  }
+	}
 }
-customElements.define('stitch-footer', Footer);
+customElements.define("stitch-footer", Footer);
 
-/// parallax backgrounds 
-window.addEventListener('scroll', () => {
-  const y = window.scrollY;
+/// parallax backgrounds
+window.addEventListener("scroll", () => {
+	const y = window.scrollY;
 
-  document.querySelector('#parallax')
-    .style.transform = `translateY(${y * 0.21}px)`;
+	document.querySelector("#parallax")
+		.style.transform = `translateY(${y * 0.21}px)`;
 });
